60b46b336cd3dc3680dcff3443737577614d7900
Kedan Zha <90431300+Zackdan0227@users.noreply.github.com>
2023-02-19 02:40:41 -0500

    basic fps

---
+++ FPS/Assets/Scripts/Character/Character_Stats.cs
@@ -0,0 +1,12 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Character_Stats : MonoBehaviour
+{
+    // Start is called before the first frame update
+   public struct PlayerStats
+{
+    public int life;
+}
+}
